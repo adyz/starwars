07408d43b7d57415175b77517b00c05afca0b7b6
Adrian Florescu <florescuadyz@yahoo.com>
2020-11-16 18:48:43 +0200

    Test function again

--- netlify_functions/starwars.js
+++ netlify_functions/starwars.js
@@ -2,11 +2,11 @@ exports.handler = async function(event, context) {
     if(event.queryStringParameters.search === 'FAIL') {
         return {
             statusCode: 500,
-            body: JSON.stringify({message: "Hello World"})
+            body: JSON.stringify({message: "Error", query: event.queryStringParameters})
         };
     }
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
+        body: JSON.stringify({message: "Hello World", query: event.queryStringParameters})
     };
 }
