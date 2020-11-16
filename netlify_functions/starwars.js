897ae6f7d2271fd2bd3266cd523f44159e8af0e7
Adrian Florescu <florescuadyz@yahoo.com>
2020-11-16 18:53:12 +0200

    Fetch forward

--- netlify_functions/starwars.js
+++ netlify_functions/starwars.js
@@ -1,12 +1,29 @@
-exports.handler = async function(event, context) {
+const fetch = require('node-fetch');
+exports.handler = async function(event, context, callback) {
     if(event.queryStringParameters.search === 'FAIL') {
         return {
             statusCode: 500,
             body: JSON.stringify({message: "Error", query: event.queryStringParameters})
         };
     }
-    return {
-        statusCode: 200,
-        body: JSON.stringify({message: "Hello World", query: event.queryStringParameters})
-    };
+
+    const checkStatus = (res) => {
+    if (res.ok) { // res.status >= 200 && res.status < 300
+        return res.json()
+    } else {
+        throw new Error(res.statusText);
+    }
+    }
+
+    try {
+        const response = await fetch(`https://swapi.dev/api/films/?search=${encodeURIComponent(event.queryStringParameters.search)}`)
+        const data = await checkStatus(response)
+        callback(null, {
+          statusCode: 200,
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(data)
+        })
+      } catch (error) {
+        callback(error)
+      }
 }
