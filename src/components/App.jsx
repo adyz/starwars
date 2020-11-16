32c1cd53337114c1e76842fed5436918b9611600
Adrian Florescu <florescuadyz@yahoo.com>
2020-11-16 14:41:45 +0200

    Added webpack tooling for local and production development

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component} from "react";
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Star Warssss</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader"
+      },
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] }, // allows import without file extension
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    port: 3000,
+    contentBase: path.resolve(__dirname, './dist'),
+    hot: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })]
+};
